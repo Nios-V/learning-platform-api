87e0844365322048713af718c9b6bd10b8d895f9
Nios-V <niosv.1104@gmail.com>
2025-10-23 22:41:41 -0300

    initial commit

---
+++ src/main/java/com/niosv/springboot/learningplatform/learning_platform_api/LearningPlatformApiApplication.java
@@ -0,0 +1,13 @@
+package com.niosv.springboot.learningplatform.learning_platform_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningPlatformApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningPlatformApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/niosv/springboot/learningplatform/learning_platform_api/LearningPlatformApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niosv.springboot.learningplatform.learning_platform_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningPlatformApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
